cd9251702b2392b87ec8cd24487d65e9db100d6c
Eren Sarıtaş <79216477+ErenSaritas6@users.noreply.github.com>
2022-06-08 21:17:07 +0300

    finished
    adding complete js file

--- script.js
+++ script.js
@@ -11,8 +11,6 @@ function computerPlay() {
     }
 }
 
-
-
 let countPlayer = 0;
 let countComputer = 0;
 
@@ -20,57 +18,89 @@ function playOneRound (playerSelection, computerSelection) {
 
     if (playerSelection == `rock` && computerSelection == `scissors`) {
         countPlayer++;
-        return `You Win! Rock beats Scissors. You: ${countPlayer} Computer: ${countComputer}`;
+        return `You Win! Rock beats Scissors.`;
     }
 
     else if (playerSelection == `paper` && computerSelection == `rock`) {
         countPlayer++;
-        return `You Win! Paper beats Rock. You: ${countPlayer} Computer: ${countComputer}`;
+        return `You Win! Paper beats Rock.`;
     }
 
     else if (playerSelection == `scissors` && computerSelection == `paper`) {
         countPlayer++;
-        return `You Win! Scissors beats Paper. You: ${countPlayer} Computer: ${countComputer}`;
+        return `You Win! Scissors beats Paper.`;
     }
 
     else if (playerSelection == `scissors` && computerSelection == `rock`) {
         countComputer++;
-        return `You Lose! Rock beats Scissors. You: ${countPlayer} Computer: ${countComputer}`;
+        return `You Lose! Rock beats Scissors.`;
     }
 
     else if (playerSelection == `rock` && computerSelection == `paper`) {
         countComputer++;
-        return `You Lose! Paper beats Rock. You: ${countPlayer} Computer: ${countComputer}`;
+        return `You Lose! Paper beats Rock.`;
     }
 
     else if (playerSelection == `paper` && computerSelection == `scissors`) {
         countComputer++;
-        return `You Lose! Scissors beats Paper. You: ${countPlayer} Computer: ${countComputer}`;
+        return `You Lose! Scissors beats Paper.`;
     }
 
     else {
-        return `It is a draw!  You: ${countPlayer} Computer: ${countComputer}`;
+        return `It is a draw!`;
     }
 }
 
-function game() {
+document.querySelector(".rock").addEventListener("click", function() {
+    let playerSelection = "rock";
+    let computerSelection = computerPlay();
+    document.querySelector(".result").innerHTML = `You: ${playerSelection} | Computer: ${computerSelection} | ${playOneRound(playerSelection, computerSelection)}`;
+    document.querySelector(".win-count").innerHTML = `You: ${countPlayer} | Computer: ${countComputer}`;
 
-        let computerSelection = computerPlay();
-        let playerSelection = ``;
-
-
-        console.log(`You: ${playerSelection}`);
-        console.log(`Computer: ${computerSelection}`);
-        console.log(`Result: ${playOneRound(playerSelection, computerSelection)}`);
-    
-
-    if (countPlayer > countComputer) {
-        console.log(`Final Result: It is ${countPlayer} to ${countComputer}. You Win!`);
+    if(countPlayer >= 5) {
+        alert(`You Won!`);
+        countComputer = 0;
+        countPlayer = 0;
     }
-    else if (countComputer > countPlayer) {
-        console.log(`Final Result: It is ${countComputer} to ${countPlayer}. You Lose!`);
+    else if(countComputer >= 5) {
+        alert(`You Lose!`);
+        countComputer = 0;
+        countPlayer = 0;
     }
-    else {
-        console.log(`Final Result: It is a draw!`);
+});
+
+document.querySelector(".paper").addEventListener("click", function() {
+    let playerSelection = "paper";
+    let computerSelection = computerPlay();
+    document.querySelector(".result").innerHTML = `You: ${playerSelection} | Computer: ${computerSelection} | ${playOneRound(playerSelection, computerSelection)}`;
+    document.querySelector(".win-count").innerHTML = `You: ${countPlayer} | Computer: ${countComputer}`;
+
+    if(countPlayer >= 5) {
+        alert(`You Won!`);
+        countComputer = 0;
+        countPlayer = 0;
     }
-}
+    else if(countComputer >= 5) {
+        alert(`You Lose!`);
+        countComputer = 0;
+        countPlayer = 0;
+    }
+});
+
+document.querySelector(".scissors").addEventListener("click", function() {
+    let playerSelection = "scissors";
+    let computerSelection = computerPlay();
+    document.querySelector(".result").innerHTML = `You: ${playerSelection} | Computer: ${computerSelection} | ${playOneRound(playerSelection, computerSelection)}`;
+    document.querySelector(".win-count").innerHTML = `You: ${countPlayer} | Computer: ${countComputer}`;
+
+    if(countPlayer >= 5) {
+        alert(`You Won!`);
+        countComputer = 0;
+        countPlayer = 0;
+    }
+    else if(countComputer >= 5) {
+        alert(`You Lose!`);
+        countComputer = 0;
+        countPlayer = 0;
+    }
+});
